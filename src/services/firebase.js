78126410d67464d71460f885263afc9f80c6d0dc
Ankit Gupta <134631639+Ankit-ai-lab@users.noreply.github.com>
2025-11-22 13:54:32 +0530

    error fix

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -34,7 +34,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getDatabase(app);
