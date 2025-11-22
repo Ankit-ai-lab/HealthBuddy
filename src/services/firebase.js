2bad7fc3dd0c48b800b7b53fb9fce26d568c49e2
Ankit Gupta <134631639+Ankit-ai-lab@users.noreply.github.com>
2025-11-22 14:15:45 +0530

    add

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,7 @@
-/* import { initializeApp } from "firebase/app";
+
+import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-import { data } from "react-router-dom";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,26 +11,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
-export const googleProvider = new GoogleAuthProvider();
- */
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCLhocXci_sIpGux9KeJlIShKX1J_o0R3A",
-  authDomain: "fir-4fe6d.firebaseapp.com",
-  databaseURL: "https://fir-4fe6d-default-rtdb.firebaseio.com",
-  projectId: "fir-4fe6d",
-  storageBucket: "fir-4fe6d.firebasestorage.app",
-  messagingSenderId: "299925830930",
-  appId: "1:299925830930:web:99d38027c14f180af4c7cb",
-  measurementId: "G-FD8EPLN9K4"
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
